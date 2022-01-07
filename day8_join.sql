e2506370febbfb5829f2da631ff7bade45b207d4
Omer Tufan <omertufanca@gmail.com>
2022-01-07 20:45:01 +0100

    07.01.22

---
+++ day8_join.sql
@@ -0,0 +1,45 @@
+use sys;
+
+/*============================== JOIN İSLEMLERİ ===============================
+    
+    Set Operatorleri (Union,Union All, {oracle->Intersect,Minus}) farklı tablolardaki sutunlari 
+    birlestirmek kullanilir.
+    
+    Join islemleri ise farklı Tablolari birlestirmek icin kullanilir. Diger 
+    bir ifade ile farkli tablolardaki secilen sutunlar ile yeni bir tablo 
+    olusturmak icin kullanilabilir.
+    
+    JOIN islemleri Iliskisel Veritabanlari icin cok onemli bir ozelliktir. Çunku
+    Foreign Key'ler ile iliskili olan tablolardan istenilen sutunlari cekmek 
+    icin JOIN islemleri kullanilabilir.
+    
+    mySQL'de 3 Cesit Join islemi kullanilabilmektedir.
+    
+    1) INNER JOIN:  Tablolardaki ortak olan sonuc kumesini gosterir
+    2) LEFT JOIN:  Ilk tabloda (Sol) olan sonuclari gosterir
+    3) RIGHT JOIN: Sadece Ikinci tabloda (sag) olan tum sonuclari gosterir
+       full JOIN=(left join + union + right join) Tablodaki tum sonuclari gosterir
+==============================================================================*/   
+    CREATE TABLE sirketler 
+    (
+        sirket_id int, 
+        sirket_isim VARCHAR(20)
+    );
+    
+    INSERT INTO sirketler VALUES(100, 'Toyota');
+    INSERT INTO sirketler VALUES(101, 'Honda');
+    INSERT INTO sirketler VALUES(102, 'Ford');
+    INSERT INTO sirketler VALUES(103, 'Hyundai');
+    
+    CREATE TABLE siparisler
+    (
+        siparis_id int,
+        sirket_id int, 
+        siparis_tarihi DATE
+    );
+    
+    INSERT INTO siparisler VALUES(11, 101, '2020-04-17');
+    INSERT INTO siparisler VALUES(22, 102, '2020-04-18');
+    INSERT INTO siparisler VALUES(33, 103, '2020-04-19');
+    INSERT INTO siparisler VALUES(44, 104, '2020-04-20');
+    INSERT INTO siparisler VALUES(55, 105, '2020-04-21');
