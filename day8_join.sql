8557941d82d6606f6f1606eba2610880cccdcff2
Omer Tufan <omertufanca@gmail.com>
2022-01-07 22:45:33 +0100

    07.01.22

--- day8_join.sql
+++ day8_join.sql
@@ -182,6 +182,113 @@ select sip.sirket_id, sir.sirket_isim, sip.siparis_id, sip.siparis_tarihi -- iki
 from sirketler sir
 right join siparisler sip
 on sir.sirket_id=sip.sirket_id;
+/* ===========================================================================================================
+==============================================================================================================
+
+===============================================================================================================
+===============================================================================================================*/
+
+CREATE TABLE bolumler (
+      bolum_id   int PRIMARY KEY,
+      bolum_isim VARCHAR(14),
+      konum      VARCHAR(13)
+    );
+    
+    INSERT INTO bolumler VALUES (10,'MUHASEBE','IST');
+    INSERT INTO bolumler VALUES (20,'MUDURLUK','ANKARA');
+    INSERT INTO bolumler VALUES (30,'SATIS','IZMIR');
+    INSERT INTO bolumler VALUES (40,'ISLETME','BURSA');
+    INSERT INTO bolumler VALUES (50,'DEPO', 'YOZGAT');
+
+    CREATE TABLE personel (
+      personel_id   int PRIMARY KEY,
+      personel_isim VARCHAR(10),
+      meslek        VARCHAR(9),
+      mudur_id      int,
+      maas          int,
+      bolum_id      int
+    );
+   
+   SELECT * FROM personel;
+  
+    INSERT INTO personel VALUES (7369,'AHMET','KATIP',1111,800,20);
+    INSERT INTO personel VALUES (7499,'BAHATTIN','SATISE',1222,1600,30);
+    INSERT INTO personel VALUES (7521,'NESE','SATISE',1222,1250,30);
+    INSERT INTO personel VALUES (1111,'MUZAFFER','MUDUR',7839,2975,20);
+    INSERT INTO personel VALUES (7654,'MUHAMMET','SATISE',1222,1250,30);
+    INSERT INTO personel VALUES (1222,'EMINE','MUDUR',7839,2850,30);
+    INSERT INTO personel VALUES (1333,'HARUN','MUDUR',7839, 2450,10);
+    INSERT INTO personel VALUES (7788,'MESUT','ANALIST',1111,3000,20);
+    INSERT INTO personel VALUES (7839,'SEHER','BASKAN',NULL,5000,10);
+    INSERT INTO personel VALUES (7844,'DUYGU','SATISE',1222,1500,30);
+    INSERT INTO personel VALUES (7876,'ALI','KATIP',1111,1100,20);
+    INSERT INTO personel VALUES (7900,'MERVE','KATIP',1222,950,30);
+    INSERT INTO personel VALUES (7902,'NAZLI','ANALIST',1111,3000,20);
+    INSERT INTO personel VALUES (7934,'EBRU','KATIP',1333,1300,10);
+    INSERT INTO personel VALUES (7956,'SIBEL','MIMAR',1333,3300,60);
+    INSERT INTO personel VALUES (7933,'ZEKI','MUHENDIS',1333,4300,60);
+
+/* -----------------------------------------------------------------------------
+  ORNEK1: SATIS ve MUHASABE bolumlerinde calisan personelin isimlerini ve 
+  bolumlerini, once bolum sonra isim sıralı olarak listeleyiniz
+------------------------------------------------------------------------------*/
+select per.personel_isim, bol.bolum_isim
+from personel per
+join bolumler bol
+on per.bolum_id=bol.bolum_id
+where bol.bolum_isim in('muhasebe' , 'satis')
+order by bol.bolum_isim, per.personel_isim;
+
+/* -----------------------------------------------------------------------------
+  ORNEK2: SATIS,ISLETME ve DEPO bolumlerinde calisan personelin isimlerini,  
+  bolumlerini  isim sıralı olarak listeleyiniz. 
+  NOT: calisani olmasa bile bolum ismi gosterilmelidir.
+------------------------------------------------------------------------------*/ 
+select p.personel_isim, b.bolum_isim
+from personel p
+right join bolumler b
+on p.bolum_id=b.bolum_id
+where b.bolum_isim in('satis', 'isletme', 'depo')
+order by p.personel_isim desc;
+
+/* -----------------------------------------------------------------------------
+  ORNEK3: Tüm bolumlerde calisan personelin isimlerini, bolum isimlerini ve 
+  maaslarini bolum ters ve maas sirali listeleyiniz. 
+  NOT: calisani olmasa bile bolum ismi gosterilmelidir.
+------------------------------------------------------------------------------*/ 
+
+select b.bolum_isim, p.personel_isim,  p.maas
+from bolumler b
+left join personel p
+on b.bolum_id=p.bolum_id
+order by b.bolum_isim desc, maas;
+
+/* -----------------------------------------------------------------------------
+  ORNEK4: SATIS ve MUDURLUK bolumlerinde calisan personelin maaslari 2000'den 
+  buyuk olanlarinin isim,bolum ve maas bilgilerini bolume ve isme gore
+  siralayarak listeleyiniz.
+------------------------------------------------------------------------------*/
+
+select p.personel_isim, b.bolum_isim, p.maas
+from bolumler b
+right join personel p
+on b.bolum_id=p.bolum_id
+where b.bolum_isim in('satis', 'mudurluk') and p.maas>2000
+order by b.bolum_isim desc, maas;
+
+/* -----------------------------------------------------------------------------
+  ORNEK5: MUDUR'u Harun veya Emine olan personelin bolumlerini,isimlerini,  
+  maaslarini ve ayrica MUDUR isimlerini 
+  (emine kimin müdürüyse onun satirinda yazsin) 
+  maas siralı olarak (Çoktan aza) listeleyiniz.
+------------------------------------------------------------------------------*/ 
+select b.bolum_isim, p.personel_isim, p.maas, (select personel_isim from personel
+                                               where p.mudur_id=personel_id              ) mudur
+from bolumler b right join personel p
+on p.bolum_id=b.bolum_id
+where p.mudur_id in(1222,1333);
+
+
 
 
 
