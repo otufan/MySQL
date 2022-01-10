1af82e8a2006ac42eb4658477d877555080ebbce
Omer Tufan <omertufanca@gmail.com>
2022-01-10 20:34:56 +0100

    10.01.22

--- HA_2.sql
+++ HA_2.sql
@@ -27,8 +27,35 @@ INSERT INTO aileler VALUES('1002', 2, 1500);
 INSERT INTO aileler VALUES('1003', 1, 2200);
 INSERT INTO aileler VALUES('1004', 3, 2400);
 
--- Maaşı ortalamanın altında olan çalışanların maaşına %20 zam yapınız.
 
+select * from calisanlar;
+select * from aileler;
+-- Veli  Han'ın  maaşına  %20  zam  yapacak  update  komutunu  yazınız.
+update calisanlar
+set maas=maas*1.2
+where isim='veli han';
 
+-- Maaşı  ortalamanın  altında  olan  çalışanların  maaşına  %20  zam  yapınız. 
 
+update calisanlar
+set maas=maas*1.2
+where maas< (select avg(maas) from (select maas from calisanlar) cls);
 
+-- Çalışanların isim ve cocuk_sayisi'ni listeleyen bir sorgu yazınız.
+
+select cls.isim, ai.cocuk_sayisi
+from calisanlar cls, aileler ai
+where cls.id=ai.id;
+
+-- calisanlar' ın id, isim ve toplam_gelir'lerini gösteren bir sorgu yazınız.
+
+select id, isim, (maas + (select ek_gelir from aileler where calisanlar.id=aileler.id)) toplam_gelir
+from calisanlar;
+
+-- Eğer bir ailenin kişi başı geliri 2000 TL den daha az ise o çalışanın
+-- maaşına ek %10 aile yardım zammı yapınız.
+-- kisi_basi_gelir = toplam_gelir / cocuk_sayisi + 2 (anne ve baba)
+
+update calisanlar
+set maas=maas*1.1
+where 2000*((select cocuk_sayisi from aileler where calisanlar.id=aileler.id)+2) > (maas + (select ek_gelir from aileler where calisanlar.id=aileler.id));
---
+++ practice.sql
@@ -0,0 +1,65 @@
+use sys;
+
+CREATE TABLE people 
+(
+    ssn CHAR (9),
+    name VARCHAR(50),
+    address VARCHAR(80)
+);
+DROP TABLE PEOPLE;
+INSERT INTO PEOPLE VALUES (123456789, 'Mark Star', 'Florida');
+INSERT INTO PEOPLE VALUES (234567890, 'Angie Way', 'Virginia');
+INSERT INTO PEOPLE VALUES (345678901, 'Marry Tien', 'New Jersey');
+INSERT INTO PEOPLE (ssn, address) VALUES (456789012, 'Michigan');
+INSERT INTO PEOPLE (ssn, address) VALUES (567890123, 'California');
+INSERT INTO PEOPLE (ssn, name) VALUES (567890123, 'California');
+SELECT * FROM PEOPLE ;
+
+-- Boş adları "ad daha sonra eklenecek" olarak değiştirin
+-- Boş adresleri 'Adres daha sonra eklenecek' olarak değiştirin
+-- people tablosunda 'Daha sonra eklenecek' tüm boş değerleri değiştirin	
+
+-- Create işçi tablosu alanları worker_id,work_name,work_salary şeklindedir.
+-- worker_id, worker_id_pk adında primary key sahip olacak
+-- İşçiler tablosuna 4 kayıt ekleyin
+-- Konsoldaki tabloya bakın
+
+CREATE TABLE worker 
+(
+    worker_id char(3),
+    worker_name VARCHAR(50),
+    worker_salary int not null,
+     CONSTRAINT worker_id_pk PRIMARY KEY (worker_id)
+);
+DROP TABLE worker;
+INSERT INTO WORKER VALUES ('100','oli can', 1200);
+INSERT INTO WORKER VALUES ('102','Veli Han', 2000);
+INSERT INTO WORKER VALUES ('103','Ayse Kan', 7000);
+INSERT INTO WORKER VALUES ('104', 'Angie Ocean', 8500);
+SELECT * FROM WORKER ;
+
+
+-- Veli Han'ın maaşını en yüksek maaştan yüzde 20 daha azına artırin
+
+update worker
+set worker_salary=(select max(worker_salary)from (select worker_salary from worker) wr)*0.8
+where worker_name='veli han';
+
+-- en dusuk maasi yuzde %30 arttirin ve ali can beyin mevcut maasi ile update edin
+
+update worker
+set worker_salary=(select min(worker_salary) from (select worker_salary from worker) wr)*1.3
+where worker_name='oli can';
+
+-- Maaş ortalama maaştan düşükse maaşları 1000 artırın
+
+update worker
+set worker_salary=worker_salary+1000
+where worker_salary<(select avg(worker_salary) from (select worker_salary from worker) wr);
+
+-- Maaşın ortalama maaştan az olması durumunda maaşları ortalama maaşa eşit
+
+update worker
+set worker_salary=(select avg(worker_salary) from (select worker_salary from worker) wr)
+where worker_salary<(select avg(worker_salary) from (select worker_salary from worker) wr);
+
